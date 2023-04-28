abdc676a91805bda76aa7982d290a192b54da4fe
Santiago Jaramillo M <santyjara11@gmail.com>
2023-04-28 14:21:44 -0500

    complete step 1

--- producers/models/producer.py
+++ producers/models/producer.py
@@ -3,7 +3,7 @@ import logging
 import time
 
 
-from confluent_kafka import avro
+from confluent_kafka import avro, CachedSchemaRegistryClient
 from confluent_kafka.admin import AdminClient, NewTopic
 from confluent_kafka.avro import AvroProducer
 
@@ -38,9 +38,8 @@ class Producer:
         #
         #
         self.broker_properties = {
-            # TODO
-            # TODO
-            # TODO
+            "broker_url":"PLAINTEXT://localhost:9092,PLAINTEXT://localhost:9093,PLAINTEXT://localhost:9094",
+            "schema_registry_url":"http://localhost:8081"
         }
 
         # If the topic does not already exist, try to create it
@@ -49,8 +48,10 @@ class Producer:
             Producer.existing_topics.add(self.topic_name)
 
         # TODO: Configure the AvroProducer
-        # self.producer = AvroProducer(
-        # )
+        self.producer = AvroProducer(
+            {"bootstrap.servers": self.broker_properties.get("broker_url")},
+            schema_registry=CachedSchemaRegistryClient(self.broker_properties.get("schema_registry_url"))
+        )
 
     def create_topic(self):
         """Creates the producer topic if it does not already exist"""
@@ -58,9 +59,13 @@ class Producer:
         #
         # TODO: Write code that creates the topic for this producer if it does not already exist on
         # the Kafka Broker.
-        #
-        #
-        logger.info("topic creation kafka integration incomplete - skipping")
+        try: 
+            topic_list = []
+            NewTopic(name=f"{self.topic_name}", num_partitions=self.num_partitions, replication_factor=self.num_replicas)
+            AdminClient.create_topics(new_topics=topic_list)
+            logger.info(f"topic creation kafka integration complete - topic: {self.topic_name} created")
+        except:
+            logger.info("topic creation kafka integration incomplete - skipping")
 
     def time_millis(self):
         return int(round(time.time() * 1000))
@@ -70,9 +75,13 @@ class Producer:
         #
         #
         # TODO: Write cleanup code for the Producer here
-        #
-        #
-        logger.info("producer close incomplete - skipping")
+        try:
+            self.producer.flush(10)
+            logger.info("producer close begin")
+        except:
+            logger.info("producer close incomplete - skipping")
+        finally:
+            logger.info(f"producer close ended with {len(self.producer)} messages still in queue")
 
     def time_millis(self):
         """Use this function to get the key for Kafka Events"""
--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -12,14 +12,13 @@ logger = logging.getLogger(__name__)
 
 
 class Turnstile(Producer):
-    key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
-
     #
     # TODO: Define this value schema in `schemas/turnstile_value.json, then uncomment the below
     #
-    #value_schema = avro.load(
-    #    f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
-    #)
+    key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
+    value_schema = avro.load(
+       f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
+    )
 
     def __init__(self, station):
         """Create the Turnstile"""
@@ -30,19 +29,13 @@ class Turnstile(Producer):
             .replace("-", "_")
             .replace("'", "")
         )
-
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
+        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of replicas
         super().__init__(
-            f"{station_name}", # TODO: Come up with a better topic name
+            topic_name=f"{station_name}_entries", # TODO: Come up with a better topic name
             key_schema=Turnstile.key_schema,
-            # TODO: value_schema=Turnstile.value_schema, TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            value_schema=Turnstile.value_schema,
+            num_partitions=self.num_partitions,
+            num_replicas=self.num_replicas,
         )
         self.station = station
         self.turnstile_hardware = TurnstileHardware(station)
@@ -51,9 +44,21 @@ class Turnstile(Producer):
         """Simulates riders entering through the turnstile."""
         num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
         logger.info("turnstile kafka integration incomplete - skipping")
-        #
-        #
-        # TODO: Complete this function by emitting a message to the turnstile topic for the number
-        # of entries that were calculated
-        #
-        #
+        # TODO: Complete this function by emitting a message to the turnstile topic for the number of entries that were calculated
+        try:
+            self.producer.produce(
+            topic=self.topic_name,
+            key={"timestamp": self.time_millis()},
+            value={
+                "station_id":self.station.station_id,
+                "station_name": self.station_name,
+                "line": self.station.color,
+                "num_entries": num_entries,
+                # TODO: Configure this
+            },
+            value_schema=self.value_schema,
+            key_schema=self.key_schema
+            )
+            logger.info("entry kafka integration creating an event")
+        except:
+            logger.info("entry kafka integration incomplete - skipping")
