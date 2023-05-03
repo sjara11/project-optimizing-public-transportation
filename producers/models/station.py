ed460475ff78c466c5a7d1d0c495fc9bd02e2ed0
Santiago Jaramillo M <santyjara11@gmail.com>
2023-05-03 11:06:21 -0500

    fix topic name to make it fixed

--- producers/models/station.py
+++ producers/models/station.py
@@ -34,7 +34,7 @@ class Station(Producer):
         # TODO: Complete the below by deciding on a topic name, number of partitions, and number of replicas
         #
         #
-        topic_name = f"{station_name}_arrivals" # TODO: Come up with a better topic name
+        topic_name = "station_arrivals" # TODO: Come up with a better topic name
         super().__init__(
             topic_name,
             key_schema=Station.key_schema,
