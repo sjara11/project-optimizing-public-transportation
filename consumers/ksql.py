937b975f34aa893562953bc750659bc156247aa2
Santiago Jaramillo M <santyjara11@gmail.com>
2023-05-03 11:06:42 -0500

    CHECKPOINT: Stage 5 reviewed

--- consumers/ksql.py
+++ consumers/ksql.py
@@ -23,14 +23,22 @@ KSQL_URL = "http://localhost:8088"
 
 KSQL_STATEMENT = """
 CREATE TABLE turnstile (
-    ???
-) WITH (
-    ???
-);
+    station_id INT,
+    station_name VARCHAR,
+    line VARCHAR
+    ) 
+WITH (
+    KAFKA_TOPIC='turnstile_entries',
+    VALUE_FORMAT='AVRO',
+    KEY='station_id'
+    )
+;
 
 CREATE TABLE turnstile_summary
-WITH (???) AS
-    ???
+WITH (VALUE_FORMAT = 'JSON') AS
+    SELECT station_id, COUNT(station_id) AS count
+    FROM turnstile
+    GROUP BY station_id;
 """
 
 
